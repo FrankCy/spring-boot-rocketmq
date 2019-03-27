b9263c0653ddf6deb57f331df84ef6e239a20959
cyoung <cy880708@163.com>
2019-03-27 19:40:16 +0800

    添加三种基本情况的消息发送

--- rocketmq-official-simple-example/provider/src/main/java/com/rmq/ose/send/config/SendAsynchronouslyMessage.java
+++ rocketmq-official-simple-example/provider/src/main/java/com/rmq/ose/send/config/SendAsynchronouslyMessage.java
@@ -1,5 +1,6 @@
 package com.rmq.ose.send.config;
 
+import com.sb.rm.common.util.DefaultMQProducerSingleton;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
@@ -27,7 +28,35 @@ public class SendAsynchronouslyMessage implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        logger.info("SendAsynchronouslyMessage");
+//        //Instantiate with a producer group name.
+//        DefaultMQProducer producer =  DefaultMQProducerSingleton.newInstance();
+//        //Launch the instance.
+//        producer.start();
+//        producer.setRetryTimesWhenSendAsyncFailed(0);
+//        for (int i = 0; i < 10; i++) {
+//            final int index = i;
+//            //Create a message instance, specifying topic, tag and message body.
+//            Message msg = new Message(
+//                    "TopicTest",
+//                    "TagB",
+//                    "OrderID188",
+//                    "Hello world".getBytes(RemotingHelper.DEFAULT_CHARSET));
+//            producer.send(msg, new SendCallback() {
+//                @Override
+//                public void onSuccess(SendResult sendResult) {
+//                    System.out.printf("%-10d OK %s %n", index,
+//                            sendResult.getMsgId());
+//                }
+//                @Override
+//                public void onException(Throwable e) {
+//                    System.out.printf("%-10d Exception %s %n", index, e);
+//                    e.printStackTrace();
+//                }
+//            });
+//        }
+//        //Shut down once the producer instance is not longer in use.
+//        producer.shutdown();
+//        logger.info("SendAsynchronouslyMessage");
     }
 
 }
--- rocketmq-official-simple-example/provider/src/main/java/com/rmq/ose/send/config/SendOneWayModeMessage.java
+++ rocketmq-official-simple-example/provider/src/main/java/com/rmq/ose/send/config/SendOneWayModeMessage.java
@@ -1,5 +1,6 @@
 package com.rmq.ose.send.config;
 
+import com.sb.rm.common.util.DefaultMQProducerSingleton;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
@@ -10,7 +11,8 @@ import org.springframework.stereotype.Component;
 
 /**
  * @version 1.0
- * @description：One-way transmission is used for cases requiring moderate reliability, such as log collection.【用于单向消息发送，在日志收集或者无需响应的情况下使用】
+ * @description：One-way transmission is used for cases requiring moderate reliability, such as log collection.
+ * 【用于单向消息发送，在日志收集或者无需响应的情况下使用】
  * @author: Yang.Chang
  * @project: spring-boot-rocketmq
  * @package: com.rmq.ose.send.config、
@@ -25,6 +27,27 @@ public class SendOneWayModeMessage implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = DefaultMQProducerSingleton.newInstance();
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 10; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message(
+                    /* Topic */
+                    "TopicTest" ,
+                    /* Tag */
+                    "TagA" ,
+                    /* Message body */
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET)
+            );
+            //Call send message to deliver message to one of brokers.
+            producer.sendOneway(msg);
+
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+
         logger.info("SendOneWayModeMessage");
     }
 }
